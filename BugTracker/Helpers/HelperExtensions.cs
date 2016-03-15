9e02d243872c9bcc28fec613a1fd41841c751b05
RiaMang <1riamanglani@gmail.com>
2016-03-15 11:15:27 -0400

    Little things
    Little things

--- BugTracker/Helpers/HelperExtensions.cs
+++ BugTracker/Helpers/HelperExtensions.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Security.Principal;
 using System.Web;
 
 namespace BugTracker.Helpers
@@ -19,6 +21,20 @@ namespace BugTracker.Helpers
     public static class HelperExtensions
     {
         private static ApplicationDbContext db = new ApplicationDbContext();
+
+        public static string GetName(this IIdentity user)
+        {
+            var ClaimsUser = (ClaimsIdentity)user;
+            var claim = ClaimsUser.Claims.FirstOrDefault(c => c.Type == "Name");
+            if(claim != null)
+            {
+                return claim.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
 
         public static ICollection<Ticket> ListTicketsForUser(this ApplicationUser user)
         {
